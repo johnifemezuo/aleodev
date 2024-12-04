670f198c9c1609c9372bb04bc194b3186cae4a84
johnifemezuo <johnifemezuoanayo@gmail.com>
2024-12-04 17:31:04 +0100

    layout changes

--- src/Components/Modules/Homepage/TestimonialSection.tsx
+++ src/Components/Modules/Homepage/TestimonialSection.tsx
@@ -1,30 +1,35 @@
 import { Container } from "@/Components/Container/Container";
-import { ITestimonial } from "../../../../base/testimonial";
-import TestimonialSlider from "../Testimonial/TestimonialSlider";
+// import { ITestimonial } from "../../../../base/testimonial";
 import SubTitle from "@/Components/Elements/SubTitle";
 import Title from "@/Components/Elements/Title";
+import EmblaCarousel from "@/Components/Carousel/EmblaCarousel";
+import { EmblaOptionsType } from 'embla-carousel'
 
 export const TestimonialSection = () => {
-  const testimonials: ITestimonial[] = [
-    {
-      name: "John Deo",
-      testimonial: "As Sales Director of Finlab Nigeria, I've seen firsthand the transformative impact our new e-commerce platform has had on our business. Tanta Innovative truly understood the unique complexities of selling scientific equipment online.",
-      logo: "/images/logo1.png",
-      title: "Director Sales and Marketing",
-    },
-    {
-      name: "John Deo",
-      testimonial: "As Sales Director of Finlab Nigeria, I've seen firsthand the transformative impact our new e-commerce platform has had on our business. Tanta Innovative truly understood the unique complexities of selling scientific equipment online.",
-      logo: "/images/logo1.png",
-      title: "Director Sales and Marketing",
-    },
-    {
-        name: "John Deo",
-        testimonial: "As Sales Director of Finlab Nigeria, I've seen firsthand the transformative impact our new e-commerce platform has had on our business. Tanta Innovative truly understood the unique complexities of selling scientific equipment online.",
-        logo: "/images/logo1.png",
-        title: "Director Sales and Marketing",
-      },
-  ];
+  const OPTIONS: EmblaOptionsType = { loop: true }
+  const SLIDE_COUNT = 5
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
+  // const testimonials: ITestimonial[] = [
+  //   {
+  //     name: "John Deo",
+  //     testimonial: "As Sales Director of Finlab Nigeria, I've seen firsthand the transformative impact our new e-commerce platform has had on our business. Tanta Innovative truly understood the unique complexities of selling scientific equipment online.",
+  //     logo: "/images/logo1.png",
+  //     title: "Director Sales and Marketing",
+  //   },
+  //   {
+  //     name: "John Deo",
+  //     testimonial: "As Sales Director of Finlab Nigeria, I've seen firsthand the transformative impact our new e-commerce platform has had on our business. Tanta Innovative truly understood the unique complexities of selling scientific equipment online.",
+  //     logo: "/images/logo1.png",
+  //     title: "Director Sales and Marketing",
+  //   },
+  //   {
+  //       name: "John Deo",
+  //       testimonial: "As Sales Director of Finlab Nigeria, I've seen firsthand the transformative impact our new e-commerce platform has had on our business. Tanta Innovative truly understood the unique complexities of selling scientific equipment online.",
+  //       logo: "/images/logo1.png",
+  //       title: "Director Sales and Marketing",
+  //     },
+  // ];
 
   return (
     <div className="w-full bg-white py-12 lg:py-32 overflow-hidden">
@@ -35,7 +40,8 @@ export const TestimonialSection = () => {
            <Title>What Our <span className="text-primary">Clients</span> Say</Title>
            </div>
 
-          <TestimonialSlider testimonials={testimonials} />
+           <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+          {/* <TestimonialSlider testimonials={testimonials} /> */}
         </div>
       </Container>
     </div>
