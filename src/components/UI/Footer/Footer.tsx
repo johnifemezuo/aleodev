0f2a9ebdafd1e54262e12f6c026677bfa826d87c
johnifemezuo <ifemezuojohn@gmail.com>
2025-01-08 17:30:07 +0100

    image fix

--- src/components/UI/Footer/Footer.tsx
+++ src/components/UI/Footer/Footer.tsx
@@ -12,7 +12,7 @@ export const Footer = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-12">
           <div className="space-y-6 :w-[400px]">
               <img
-                src="/images/aleodev-logo.png"
+                src="/images/aleodev-white.png"
                 alt=""
                 className="w-[140px] lg:w-40"
               />
